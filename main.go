713c0b8f891c4b31cd1241e3dd0097e9c047ca6e
J.-Fabian Wenisch <fabian@wenisch.tech>
2021-10-07 18:56:02 +0200

    fix: Added running duration

--- main.go
+++ main.go
@@ -4,15 +4,23 @@ import (
 	"os"
 	"bytes"
 	"fmt"
+	"time"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
+
+	for{
+
+	fmt.Println("###################### STARTING ######################")
 	url := os.Getenv("AHTTP_URL")
 	json := os.Getenv("AHTTP_BODY")
 	method := os.Getenv("AHTTP_METHOD")
 	contenttype := os.Getenv("AHTTP_CONTENTTYPE")
+	initialWait := os.Getenv("AHTTP_WAITTIME")
+	fmt.Println("Wait:>", initialWait)
+	wait,_ := time.ParseDuration(initialWait)
 
 	fmt.Println("URL:>", url)
 	fmt.Println("BODY:>", json)
@@ -33,4 +41,7 @@ func main() {
 	fmt.Println("response Headers:", resp.Header)
 	body, _ := ioutil.ReadAll(resp.Body)
 	fmt.Println("response Body:", string(body))
+	fmt.Println("Going to sleep for "+initialWait)
+	time.Sleep(wait)
+}
 }
